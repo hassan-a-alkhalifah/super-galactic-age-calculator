6f0bfbfaf7df8e7a48658074de8a7b64bcc906be
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-05 10:09:11 -0700

    add constructor UserData

---
+++ src/js/backend.js
@@ -0,0 +1,14 @@
+// Backend Logic
+export function UserData() {
+  this.age;
+  this.weight;
+  this.height;
+  this.education;
+  this.relationshipStatus;
+  this.workingStatus;
+  this.income;
+  this.exercise;
+  this.diet;
+  this.alcohol;
+  this.smoking;
+}
