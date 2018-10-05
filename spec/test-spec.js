e80e382c2cbd787656b3357d678aeec94e75116e
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-05 15:21:26 -0700

    add getYearsLivedAfterLifeExpectancyPerPlanet prototype which checks if user has lived past average life expectancy, and if so, will provide how many years they lived past average life expectancy per planet

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -17,9 +17,105 @@ describe("UserData", function() {
         return "4";
       }
     }
+    UserData.prototype.getYearsLeftToLiverPerPlanet = function() {
+      let weightType = this.getWeightType();
+      let averageLifeExpectancy = this.sex;
+
+      if(weightType == "1" || weightType == "3") {
+        averageLifeExpectancy -= 5;
+      } else if(weightType == "4") {
+        averageLifeExpectancy -= 20;
+      }
+
+      if(this.education == "1") {
+        averageLifeExpectancy -= 1;
+      } else if(this.education == "3") {
+        averageLifeExpectancy += 1;
+      } else if(this.education == "4") {
+        averageLifeExpectancy += 2;
+      }
+
+      if(this.relationshipStatus == "2") {
+        averageLifeExpectancy += 2;
+      } else if(this.relationshipStatus == "3") {
+        averageLifeExpectancy -= 2;
+      } else if(this.relationshipStatus == "4") {
+        averageLifeExpectancy -= 1;
+      }
+
+      if(this.workingStatus == "1") {
+        averageLifeExpectancy += 2;
+      } else if(this.workingStatus == "2") {
+        averageLifeExpectancy += 1;
+      } else if(this.workingStatus == "4") {
+        averageLifeExpectancy -= 2;
+      }
+
+      if(this.income == "1" || this.income == "3") {
+        averageLifeExpectancy += 1;
+      } else if(this.income == "4") {
+        averageLifeExpectancy += 2;
+      }
+
+      if(this.exercise == "1") {
+        averageLifeExpectancy -= 5;
+      } else if(this.exercise == "2") {
+        averageLifeExpectancy += 1;
+      } else if(this.exercise == "3") {
+        averageLifeExpectancy += 2;
+      } else {
+        averageLifeExpectancy += 4;
+      }
+
+      if(this.diet == "1") {
+        averageLifeExpectancy -= 5;
+      } else if(this.diet == "3") {
+        averageLifeExpectancy += 1;
+      } else if(this.diet == "4") {
+        averageLifeExpectancy += 2;
+      } else if(this.diet == "5") {
+        averageLifeExpectancy += 4;
+      }
+
+      if(this.alcohol == "1") {
+        averageLifeExpectancy += 1;
+      } else if(this.alcohol == "3") {
+        averageLifeExpectancy -= 2;
+      } else if(this.alcohol == "4") {
+        averageLifeExpectancy -= 4;
+      }
+
+      if(this.smoking == "1") {
+        averageLifeExpectancy += 1;
+      } else if(this.smoking == "2") {
+        averageLifeExpectancy -= 1;
+      } else {
+        averageLifeExpectancy -= 2;
+      }
+
+      this.expectedAgeOfDeathOnEarth = parseFloat((averageLifeExpectancy).toFixed(2));
+      this.expectedAgeOfDeathOnMercury = parseFloat(((averageLifeExpectancy / .24)).toFixed(2));
+      this.expectedAgeOfDeathOnVenus = parseFloat(((averageLifeExpectancy / .62)).toFixed(2));
+      this.expectedAgeOfDeathOnMars = parseFloat(((averageLifeExpectancy / 1.88)).toFixed(2));
+      this.expectedAgeOfDeathOnJupiter = parseFloat(((averageLifeExpectancy / 11.86)).toFixed(2));
+    }
     newUser = new UserData();
     newUser.weight = 195;
     newUser.height = 70;
+    newUser.age = 100;
+    newUser.mercuryAge = 416.67;
+    newUser.venusAge = 161.29;
+    newUser.marsAge = 53.19;
+    newUser.jupiterAge = 8.43;
+    newUser.sex = 83;
+    newUser.education = "2";
+    newUser.relationshipStatus = "2";
+    newUser.workingStatus = "2";
+    newUser.income = "1";
+    newUser.exercise = "4";
+    newUser.diet = "5";
+    newUser.alcohol = "1";
+    newUser.smoking = "1";
   });
 
   it("Store user input in new object based on key value pairs or constructor UserData", function() {
@@ -178,11 +274,11 @@ describe("UserData", function() {
         averageLifeExpectancy -= 2;
       }
 
-      newUser.yearsLeftToLiveOnEarth = (averageLifeExpectancy).toFixed(2);
-      newUser.yearsLeftToLiveOnMercury = ((averageLifeExpectancy / .24)).toFixed(2);
-      newUser.yearsLeftToLiveOnVenus = ((averageLifeExpectancy / .62)).toFixed(2);
-      newUser.yearsLeftToLiveOnMars = ((averageLifeExpectancy / 1.88)).toFixed(2);
-      newUser.yearsLeftToLiveOnJupiter = ((averageLifeExpectancy / 11.86)).toFixed(2);
+      newUser.expectedAgeOfDeathOnEarth = parseFloat((averageLifeExpectancy).toFixed(2));
+      newUser.expectedAgeOfDeathOnMercury = parseFloat(((averageLifeExpectancy / .24)).toFixed(2));
+      newUser.expectedAgeOfDeathOnVenus = parseFloat(((averageLifeExpectancy / .62)).toFixed(2));
+      newUser.expectedAgeOfDeathOnMars = parseFloat(((averageLifeExpectancy / 1.88)).toFixed(2));
+      newUser.expectedAgeOfDeathOnJupiter = parseFloat(((averageLifeExpectancy / 11.86)).toFixed(2));
     }
     newUser.age = 30.74;
     newUser.mercuryAge = 128.08;
@@ -199,11 +295,36 @@ describe("UserData", function() {
     newUser.alcohol = "1";
     newUser.smoking = "1";
     newUser.getYearsLeftToLiverPerPlanet();
-    expect(newUser.yearsLeftToLiveOnEarth).toEqual("92.00");
-    expect(newUser.yearsLeftToLiveOnMercury).toEqual("383.33");
-    expect(newUser.yearsLeftToLiveOnVenus).toEqual("148.39");
-    expect(newUser.yearsLeftToLiveOnMars).toEqual("48.94");
-    expect(newUser.yearsLeftToLiveOnJupiter).toEqual("7.76");
+    expect(newUser.expectedAgeOfDeathOnEarth).toEqual(92.00);
+    expect(newUser.expectedAgeOfDeathOnMercury).toEqual(383.33);
+    expect(newUser.expectedAgeOfDeathOnVenus).toEqual(148.39);
+    expect(newUser.expectedAgeOfDeathOnMars).toEqual(48.94);
+    expect(newUser.expectedAgeOfDeathOnJupiter).toEqual(7.76);
+  });
+
+  it("If user lives after life expectancy, get years lived after life expectancy per planet ", function() {
+    let test;
+    UserData.prototype.getYearsLivedAfterLifeExpectancyPerPlanet = function() {
+      if(this.age > this.expectedAgeOfDeathOnEarth) {
+        this.yearsLivedAfterExpectedAgeOfDeathOnEarth = (this.age - this.expectedAgeOfDeathOnEarth).toFixed(2);
+
+        this.yearsLivedAfterExpectedAgeOfDeathOnMercury = (this.mercuryAge - this.expectedAgeOfDeathOnMercury).toFixed(2);
+
+        this.yearsLivedAfterExpectedAgeOfDeathOnVenus = (this.venusAge - this.expectedAgeOfDeathOnVenus).toFixed(2);
+
+        this.yearsLivedAfterExpectedAgeOfDeathOnMars = (this.marsAge - this.expectedAgeOfDeathOnMars).toFixed(2);
+
+        this.yearsLivedAfterExpectedAgeOfDeathOnJupiter = (this.jupiterAge - this.expectedAgeOfDeathOnJupiter).toFixed(2);
+      }
+    }
+    newUser.getWeightType();
+    newUser.getYearsLeftToLiverPerPlanet();
+    newUser.getYearsLivedAfterLifeExpectancyPerPlanet();
+    expect(newUser.yearsLivedAfterExpectedAgeOfDeathOnEarth).toEqual("8.00");
+    expect(newUser.yearsLivedAfterExpectedAgeOfDeathOnMercury).toEqual("33.34");
+    expect(newUser.yearsLivedAfterExpectedAgeOfDeathOnVenus).toEqual("12.90");
+    expect(newUser.yearsLivedAfterExpectedAgeOfDeathOnMars).toEqual("4.25");
+    expect(newUser.yearsLivedAfterExpectedAgeOfDeathOnJupiter).toEqual("0.67");
   });
 
 });
