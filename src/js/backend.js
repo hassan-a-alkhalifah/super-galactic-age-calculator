f12c26a466a68b97a60d88710606db22facf6e0e
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-05 12:30:14 -0700

    remove parseInt() from getUserCurrentAge prototypes final age calculation, add getUserVariousPlanetAge prototype

--- src/js/backend.js
+++ src/js/backend.js
@@ -18,5 +18,12 @@ UserData.prototype.getUserCurrentAge = function(year, month, day) {
   let birthDateMilliseconds = birthDate.getTime();
   let ageMilliseconds = Date.now() - birthDateMilliseconds;
   let millisecondsPerYear = 31536000000;
-  this.age = parseFloat((ageMilliseconds/millisecondsPerYear).toFixed(2));
+  this.age = (ageMilliseconds/millisecondsPerYear).toFixed(2);
+}
+
+UserData.prototype.getUserVariousPlanetAge = function() {
+  this.MercuryAge = (this.age / .24).toFixed(2);
+  this.VenusAge = (this.age / .62).toFixed(2);
+  this.MarsAge = (this.age / 1.88).toFixed(2);
+  this.JupiterAge = (this.age / 11.86).toFixed(2);
 }
