4b4a496ca7d77590c99277400485dbedcbf18bb8
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-05 12:59:43 -0700

    add parseFloat to planet equations in getUserVariousPlanetAge prototype, add getWeightType prototype to determine if user is underweight, normal weight, overweight or obese

--- spec/test-spec.js
+++ spec/test-spec.js
@@ -3,8 +3,9 @@ import { UserData } from "./../src/js/backend.js";
 describe("UserData", function() {
 
   it("Store user input in new object based on key value pairs or constructor UserData", function() {
-    let newUser = new UserData;
+    let newUser = new UserData();
     newUser.age = 30;
+    newUser.sex = 83;
     newUser.weight = 195;
     newUser.height = 70;
     newUser.education = "2";
@@ -16,6 +17,7 @@ describe("UserData", function() {
     newUser.alcohol = "1";
     newUser.smoking = "1";
     expect(newUser.age).toEqual(30);
+    expect(newUser.sex).toEqual(83);
     expect(newUser.weight).toEqual(195);
     expect(newUser.height).toEqual(70);
     expect(newUser.education).toEqual("2");
@@ -28,7 +30,7 @@ describe("UserData", function() {
     expect(newUser.smoking).toEqual("1");
   });
 
-  it("get users current date of birth, i.e month, day and year, to calculate user's current age", function() {
+  it("Get users current date of birth, i.e month, day and year, to calculate user's current age", function() {
     UserData.prototype.getUserCurrentAge = function(year, month, day) {
       let birthDate = new Date(year, month, day);
       let birthDateMilliseconds = birthDate.getTime();
@@ -36,25 +38,45 @@ describe("UserData", function() {
       let millisecondsPerYear = 31536000000;
       this.age = (ageMilliseconds/millisecondsPerYear).toFixed(2);
     }
-    let newUser = new UserData;
+    let newUser = new UserData();
     newUser.getUserCurrentAge(1987, 12, 16);
     expect(newUser.age).toEqual("30.74");
   });
 
-  it("convert users Earth age into Mercury, Venus, Mars and Jupiter years", function() {
+  it("Convert users Earth age into Mercury, Venus, Mars and Jupiter years", function() {
     UserData.prototype.getUserVariousPlanetAge = function() {
-      this.MercuryAge = (this.age / .24).toFixed(2);
-      this.VenusAge = (this.age / .62).toFixed(2);
-      this.MarsAge = (this.age / 1.88).toFixed(2);
-      this.JupiterAge = (this.age / 11.86).toFixed(2);
+      this.mercuryAge = parseFloat((this.age / .24).toFixed(2));
+      this.venusAge = parseFloat((this.age / .62).toFixed(2));
+      this.marsAge = parseFloat((this.age / 1.88).toFixed(2));
+      this.jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
     }
-    let newUser = new UserData;
+    let newUser = new UserData();
     newUser.age = 30.74;
     newUser.getUserVariousPlanetAge();
-    expect(newUser.MercuryAge).toEqual("128.08");
-    expect(newUser.VenusAge).toEqual("49.58");
-    expect(newUser.MarsAge).toEqual("16.35");
-    expect(newUser.JupiterAge).toEqual("2.59");
+    expect(newUser.mercuryAge).toEqual(128.08);
+    expect(newUser.venusAge).toEqual(49.58);
+    expect(newUser.marsAge).toEqual(16.35);
+    expect(newUser.jupiterAge).toEqual(2.59);
+  });
+
+  it("Determine users weight type", function() {
+    UserData.prototype.getWeightType = function() {
+      let BMI = (newUser.weight * 0.45)/((newUser.height * 0.025)*(newUser.height * 0.025));
+      if(BMI < 18.5) {
+        return 1;
+      } else if(BMI >= 18.5 && BMI <= 24.9) {
+        return 2;
+      } else if(BMI >= 25 && BMI <= 29.9) {
+        return 3;
+      } else {
+        return 4;
+      }
+    }
+    let newUser = new UserData();
+    newUser.weight = 195;
+    newUser.height = 70;
+    let BMI = newUser.getWeightType();
+    expect(BMI).toEqual(3);
   });
 
 });
