fcfdd5460e61ea3d131529aeb612be51e45d5e87
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-05 18:04:41 -0700

    create new object newUser, take user input values and store in new object, add prototype calls, add console of newly updated object with output results based on user input

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,34 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../styles.css';
+import { UserData } from "./backend.js";
 
 $(document).ready(function() {
-  
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+    let newUser = new UserData();
+    let month = parseInt($("#month").val());
+    let day = parseInt($("#day").val());
+    let year = parseInt($("#year").val());
+    newUser.sex = parseInt($("input[name=gender]:checked").val());
+    newUser.weight = parseInt($("#weight").val());
+    newUser.height = (parseInt($("#feet").val()) * 12) + parseInt($("#inch").val());
+    newUser.education = $("input[name=education]:checked").val();
+    newUser.relationshipStatus = $("input[name=relationship]:checked").val();
+    newUser.workingStatus = $("input[name=working]:checked").val();
+    newUser.incomes = $("input[name=income]:checked").val();
+    newUser.exercise = $("input[name=exercise]:checked").val();
+    newUser.diet = $("input[name=health]:checked").val();
+    newUser.alcohol = $("input[name=alcohol]:checked").val();
+    newUser.smoking = $("input[name=smoking]:checked").val();
+    newUser.getUserCurrentAge(year, month, day);
+    newUser.getUserVariousPlanetAge();
+    newUser.getWeightType();
+    newUser.getYearsLeftToLiverPerPlanet();
+    if(this.age > this.expectedAgeOfDeathOnEarth) {
+      newUser.getYearsLivedAfterLifeExpectancyPerPlanet();
+    }
+    console.log(newUser);
+  });
 });
