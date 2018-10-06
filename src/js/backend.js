bae8368f6f65e794ca610867963b081248fe8c76
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-05 18:01:18 -0700

    convert return value in getWeightType prototype from integer to string

--- src/js/backend.js
+++ src/js/backend.js
@@ -20,27 +20,27 @@ UserData.prototype.getUserCurrentAge = function(year, month, day) {
   let ageMilliseconds = Date.now() - birthDateMilliseconds;
   let millisecondsPerYear = 31536000000;
   this.age = (ageMilliseconds/millisecondsPerYear).toFixed(2);
-}
+};
 
 UserData.prototype.getUserVariousPlanetAge = function() {
   this.mercuryAge = parseFloat((this.age / .24).toFixed(2));
   this.venusAge = parseFloat((this.age / .62).toFixed(2));
   this.marsAge = parseFloat((this.age / 1.88).toFixed(2));
   this.jupiterAge = parseFloat((this.age / 11.86).toFixed(2));
-}
+};
 
 UserData.prototype.getWeightType = function() {
   let BMI = (this.weight * 0.45)/((this.height * 0.025)*(this.height * 0.025));
   if(BMI < 18.5) {
-    return 1;
+    return "1";
   } else if(BMI >= 18.5 && BMI <= 24.9) {
-    return 2;
+    return "2";
   } else if(BMI >= 25 && BMI <= 29.9) {
-    return 3;
+    return "3";
   } else {
-    return 4;
+    return "4";
   }
-}
+};
 
 UserData.prototype.getYearsLeftToLiverPerPlanet = function() {
   let weightType = this.getWeightType();
@@ -113,7 +113,7 @@ UserData.prototype.getYearsLeftToLiverPerPlanet = function() {
   this.expectedAgeOfDeathOnVenus = parseFloat(((averageLifeExpectancy / .62)).toFixed(2));
   this.expectedAgeOfDeathOnMars = parseFloat(((averageLifeExpectancy / 1.88)).toFixed(2));
   this.expectedAgeOfDeathOnJupiter = parseFloat(((averageLifeExpectancy / 11.86)).toFixed(2));
-}
+};
 
 UserData.prototype.getYearsLivedAfterLifeExpectancyPerPlanet = function() {
   this.yearsLivedAfterExpectedAgeOfDeathOnEarth = (this.age - this.expectedAgeOfDeathOnEarth).toFixed(2);
@@ -125,4 +125,4 @@ UserData.prototype.getYearsLivedAfterLifeExpectancyPerPlanet = function() {
   this.yearsLivedAfterExpectedAgeOfDeathOnMars = (this.marsAge - this.expectedAgeOfDeathOnMars).toFixed(2);
 
   this.yearsLivedAfterExpectedAgeOfDeathOnJupiter = (this.jupiterAge - this.expectedAgeOfDeathOnJupiter).toFixed(2);
-}
+};
