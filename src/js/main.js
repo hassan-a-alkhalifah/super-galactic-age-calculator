48aaf8285a37c8ce976811679baddd3116fa7773
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-07 13:16:59 -0700

    import /scss/styles.scss

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import '../styles.css';
+import '../scss/styles.scss';
 import { UserData } from "./backend.js";
 
 $(document).ready(function() {
