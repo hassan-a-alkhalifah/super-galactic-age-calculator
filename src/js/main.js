3ac0e7cba56426ff1d06825abcff9c6fdbb318c2
Hassan Al-khalifah <hassan.a.alkhalifah@gmail.com>
2018-10-11 22:17:02 -0700

    renamed file to UserData.js

--- src/js/main.js
+++ src/js/main.js
@@ -2,27 +2,26 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../scss/styles.scss';
-import { UserData } from "./backend.js";
-
+import UserData from "./UserData.js";
 $(document).ready(function() {
+  const test = new UserData();
   $("#form").submit(function(event) {
     event.preventDefault();
-
-    let newUser = new UserData();
-    let month = parseInt($("#month").val());
-    let day = parseInt($("#day").val());
-    let year = parseInt($("#year").val());
-    newUser.sex = parseInt($("input[name=gender]:checked").val());
-    newUser.weight = parseInt($("#weight").val());
-    newUser.height = (parseInt($("#feet").val()) * 12) + parseInt($("#inch").val());
-    newUser.education = $("input[name=education]:checked").val();
-    newUser.relationshipStatus = $("input[name=relationship]:checked").val();
-    newUser.workingStatus = $("input[name=working]:checked").val();
-    newUser.incomes = $("input[name=income]:checked").val();
-    newUser.exercise = $("input[name=exercise]:checked").val();
-    newUser.diet = $("input[name=health]:checked").val();
-    newUser.alcohol = $("input[name=alcohol]:checked").val();
-    newUser.smoking = $("input[name=smoking]:checked").val();
+    const month = parseInt($("#month").val());
+    const day = parseInt($("#day").val());
+    const year = parseInt($("#year").val());
+    const sex = parseInt($("input[name=gender]:checked").val());
+    const weight = parseInt($("#weight").val());
+    const height = (parseInt($("#feet").val()) * 12) + parseInt($("#inch").val());
+    const education = $("input[name=education]:checked").val();
+    const relationshipStatus = $("input[name=relationship]:checked").val();
+    const workingStatus = $("input[name=working]:checked").val();
+    const incomes = $("input[name=income]:checked").val();
+    const exercise = $("input[name=exercise]:checked").val();
+    const diet = $("input[name=health]:checked").val();
+    const alcohol = $("input[name=alcohol]:checked").val();
+    const smoking = $("input[name=smoking]:checked").val();
+    const newUser = new UserData(sex, weight, height, education, relationshipStatus, workingStatus, incomes, exercise, diet, alcohol, smoking);
     newUser.getUserCurrentAge(year, month, day);
     newUser.getUserVariousPlanetAge();
     newUser.getWeightType();
